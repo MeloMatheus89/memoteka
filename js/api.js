4a559ac0a9e2210295980f2fe4a6fb477be24706
Matheus Melo <matheusmelo@hotmail.com.br>
2025-06-04 20:56:54 -0300

    requisição post concluída Correções no endereço das imagens das aspas (funcionando)

--- js/api.js
+++ js/api.js
@@ -9,6 +9,23 @@ const api = {
       throw error;
     }
   },
+    async salvarUmPensamento(pensamento) {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      });
+
+      return response.json();
+    } catch {
+      alert("Erro ao buscar pensamentos");
+      throw error;
+    }
+  },
+  
 };
 
 export default api;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.rederizarPensamentos();
 });
+
+const formularioPensamento = document.getElementById("pensamento-form");
+formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario());
+
+async function manipularSubmissaoFormulario(event) {
+  event.preventDefault();
+  const id = document.getElementById("pensamento-id").value;
+  const pensamento = document.getElementById("pensamento-conteudo").value;
+  const autoria = document.getElementById("pensamento-autor").value;
+
+  try {
+    await api.salvarUmPensamento({ pensamento, autoria });
+    ui.rederizarPensamentos();
+  } catch (error) {
+    alert(`Um erro ocorreu ao escrever no formulário: ${error}`);
+  }
+}
